543b0f168c74bb5df82801e55c2a594efa93d73b
Christian Bieniak <cbieniak@gmail.com>
2015-04-28 21:11:44 +1000

    Initial Commit

---
+++ BatteryGlance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BatteryGlance
+//
+//  Created by Christian Bieniak on 28/04/2015.
+//  Copyright (c) 2015 BienProgramming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BatteryGlanceTests/BatteryGlanceTests.m
@@ -0,0 +1,40 @@
+//
+//  BatteryGlanceTests.m
+//  BatteryGlanceTests
+//
+//  Created by Christian Bieniak on 28/04/2015.
+//  Copyright (c) 2015 BienProgramming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BatteryGlanceTests : XCTestCase
+
+@end
+
+@implementation BatteryGlanceTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
